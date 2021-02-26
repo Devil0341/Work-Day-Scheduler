9bfb9697cd731c4743d019a0f935e1d03945b5e2
Sureshot0341 <gregpetropoulos@yahoo.com>
2021-02-25 23:26:51 -0500

    I have am working on the function for the background colors according to the current time, once finished I  will move on to the local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,7 @@ var timeBlocks = $('.container');
 var userInputRow = $('.description');
 var timeBlockRow = $('#time-block');
 var rowHour = $('.hour');
-// var presentClass = $('.present');
-// var pastClass = $('.past');
-// var futureClass = $('.future')
+
 var isPresent;
 var isPast;
 var isFuture;
@@ -15,18 +13,36 @@ var isFuture;
 var todayDate = moment().format('dddd, MMMM Do');
 $('#currentDay').text(todayDate);
 
-//Make a function use jquery to change color of time blocks according to date 
+///jquery function to loop over time blocks
 $(function colorDate() {
-    //set css class .present to row if it's today date
     //current time now variable
     var currentHour = moment().hour()
-    for (var i = 0; i < timeBlockRow.length; i++){
-        if (rowHour == currentHour){
-        userInputRow.addClass('.present');
-        isPresent = true;
-        }else (rowHour < currentHour)
-    }
+
+    //when looping use `this` to refer to the current html element(time-block)
+    // Military time will be read 0900-1700
+    timeBlockRow.each(function (i) {
+        //turn html element #time-blocks into a number, set attribute id, and split each number into an array
+        var blockHour = parseInt($(this).attr("id").split("time-block")[1]);
+
+        //check time and set appropriate rows to background color
+        if (currentHour === blockHour) {
+            $(this).addClass('present')
+
+        } else if (currentHour > blockHour) {
+            $(this).addClass('past')
+        } else {
+            $(this).addClass('future')
+        }
+    });
 });
 
 
-//save button function
+
+
+//Once save button is clicked the function will save user input into local storage and show back up after refreshed page
+
+$(function saveButton() {
+    //need on(click)
+    //need to set item
+    //need to get item on page refresh
+});
